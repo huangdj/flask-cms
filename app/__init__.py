0de3c97263cc119fe6c7a153119e7b707e16ed69
huangdj <244500972@qq.com>
2021-03-11 14:41:10 +0800

    配置session 和 Redis

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from config import config
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_session import Session  # 导入session扩展
 
 app = Flask(__name__)
 db = SQLAlchemy()
@@ -13,5 +13,6 @@ def create_app(config_name):
     app.config.from_object(config[config_name])  # 加载配置文件挪到工厂函数中
 
     db.init_app(app)  # 通过 init_app 去实例化db对象
+    Session(app)  # 实例化Session
 
     return app  # 最后返回当前app
--- manage.py
+++ manage.py
@@ -1,3 +1,4 @@
+from flask import session
 from flask_script import Manager  # 导入扩展，可以使用管理器启动项目
 from flask_migrate import MigrateCommand, Migrate  # 导入迁移包
 from app import create_app, db  # 导入db函数
@@ -11,7 +12,8 @@ manage.add_command('db', MigrateCommand)  # 添加迁移命令
 
 @app.route('/')
 def index():
-    return 'Hello Clwy!'
+    session['name'] = 'clwy'  # 设置一个假的 session
+    return session['name']
 
 
 if __name__ == '__main__':
